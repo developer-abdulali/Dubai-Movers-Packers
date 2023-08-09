feaba8583e4d0325c9d4f12508f80ba712c2445a
developer-abdulali <ab.ali.soomro@gmail.com>
2023-08-10 00:22:31 +0500

    added changes

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -91,9 +91,6 @@
 
 // export default Navbar;
 
-
-
-
 import React, { useState } from "react";
 
 const Navbar = () => {
@@ -105,7 +102,10 @@ const Navbar = () => {
 
   return (
     <nav>
-      
+      <div className="hidden bg-[#0772BD] text-3xl h-14 md:flex items-center justify-center">
+        <span className="text-[#ffffff]">وَاللَّهُ خَيْرُ الرَّازِقِينَ</span>
+      </div>
+
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 mt-4">
         <div className="flex items-center justify-between h-16">
           <div className="flex-shrink-0">
@@ -114,7 +114,7 @@ const Navbar = () => {
                 src="/images/logo-removebg.png"
                 alt="logo"
                 className="w-[120px] sm:w-[200px]"
-                />
+              />
             </a>
           </div>
           <div className="hidden md:flex ml-10 space-x-6 text-md font-medium">
@@ -142,7 +142,7 @@ const Navbar = () => {
               onClick={toggleMenu}
               type="button"
               className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 focus:ring-offset-gray-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-white"
-              >
+            >
               <span className="sr-only">Open main menu</span>
               <svg
                 className={`${isOpen ? "hidden" : "block"} h-6 w-6`}
@@ -151,13 +151,13 @@ const Navbar = () => {
                 viewBox="0 0 24 24"
                 stroke="currentColor"
                 aria-hidden="true"
-                >
+              >
                 <path
                   strokeLinecap="round"
                   strokeLinejoin="round"
                   strokeWidth="2"
                   d="M4 6h16M4 12h16M4 18h16"
-                  />
+                />
               </svg>
               <svg
                 className={`${isOpen ? "block" : "hidden"} h-6 w-6`}
@@ -166,13 +166,13 @@ const Navbar = () => {
                 viewBox="0 0 24 24"
                 stroke="currentColor"
                 aria-hidden="true"
-                >
+              >
                 <path
                   strokeLinecap="round"
                   strokeLinejoin="round"
                   strokeWidth="2"
                   d="M6 18L18 6M6 6l12 12"
-                  />
+                />
               </svg>
             </button>
           </div>
@@ -182,7 +182,7 @@ const Navbar = () => {
         className={`${
           isOpen ? "block" : "hidden"
         } md:hidden px-2 pt-2 pb-3 mt-5 bg-black text-white text-center`}
-        >
+      >
         <div className="flex flex-col space-y-5 text-lg font-medium">
           <a href="#" className="duration-200 hover:text-cyan-300">
             Home
@@ -206,11 +206,3 @@ const Navbar = () => {
 };
 
 export default Navbar;
-
-
-
-
-
-//   <div className="hidden bg-[#0772BD] text-3xl h-14 md:flex items-center justify-center">
-//   <span className="text-[#ffffff]">وَاللَّهُ خَيْرُ الرَّازِقِينَ</span>
-// </div>
