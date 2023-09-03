459b539889339aa76823e55959f12b30f1aba2ec
developer-abdulali <ab.ali.soomro@gmail.com>
2023-09-03 17:28:32 +0500

    added changes

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -1,10 +1,9 @@
 import React, { useEffect } from "react";
 import AOS from "aos";
-import "aos/dist/aos.css"; // Import the AOS CSS file
-
+import "aos/dist/aos.css"; 
 const Carousel = () => {
   useEffect(() => {
-    AOS.init(); // Initialize AOS
+    AOS.init();
   }, []);
 
   return (
