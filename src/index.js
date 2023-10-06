25c248cdef228062d8a3c56f9951be01f5bcc3d6
developer-abdulali <ab.ali.soomro@gmail.com>
2023-10-06 21:27:35 +0500

    added loader

--- src/index.js
+++ src/index.js
@@ -1,17 +1,52 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import ArabicText from "./components/ArabicText/ArabicText";
 import Footer from "./components/Footer/Footer";
 import WhatsappContact from "./components/WhatsappContact/WhatsappContact";
+import Loader from "./components/Loader/Loader";
+
+const Root = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // Simulate loading data (replace with your actual data fetching logic)
+    const fetchData = async () => {
+      try {
+        // Set loading to true when data fetching starts
+        setLoading(true);
+
+        // Simulate data fetching delay (replace with actual data fetching logic)
+        await new Promise((resolve) => setTimeout(resolve, 2000));
+
+        // Set loading to false when data fetching is complete
+        setLoading(false);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []); // Run this effect only once on component mount
+
+  return (
+    <React.StrictMode>
+      {loading ? (
+        <Loader /> // Display the loader if data is still loading
+      ) : (
+        // Render your actual content once data is loaded
+        <>
+          <ArabicText />
+          <App />
+          <Footer />
+          <WhatsappContact />
+        </>
+      )}
+    </React.StrictMode>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <ArabicText />
-    <App />
-    <Footer />
-    <WhatsappContact />
-  </React.StrictMode>
-);
+root.render(<Root />);
